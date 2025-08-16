bc50a581a36eacb2a7a56886f32f396e3da8406a
raashed <mohammedraashed10@gmail.com>
2025-08-16 15:19:23 +0530

    fix: removed the userCount

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,15 +2,11 @@ import { WebSocketServer, WebSocket } from 'ws';
 
 const wss = new WebSocketServer({port: 8080});
 
-let userCount = 0;
 let allSockets: WebSocket[] = [];
 
 wss.on('connection', (socket) => {
 
     allSockets.push(socket);
-    
-    userCount++;
-    console.log(`user connected #${userCount}`);
 
     socket.on('message', (message) => {
 
