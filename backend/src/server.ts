515897f0cf453a2366f61f7a087005457d988799
raashed <mohammedraashed10@gmail.com>
2025-08-16 14:54:40 +0530

    chore: backend v1 done

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,1 +1,21 @@
-console.log('chat app init!');
+import { WebSocketServer, WebSocket } from 'ws';
+
+const wss = new WebSocketServer({port: 8080});
+
+let userCount = 0;
+let allSockets: WebSocket[] = [];
+
+wss.on('connection', (socket) => {
+
+    allSockets.push(socket);
+    
+    userCount++;
+    console.log(`user connected #${userCount}`);
+
+    socket.on('message', (message) => {
+        for ( let i = 0; i < allSockets.length; i++ ) {
+            const s = allSockets[i];
+            s?.send(message.toString());
+        }
+    });
+});
