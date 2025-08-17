753abe06cbbabb7941426f81d2bf68dc7c9f044f
raashed <mohammedraashed10@gmail.com>
2025-08-17 09:28:53 +0530

    fix: update the server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,16 +2,39 @@ import { WebSocketServer, WebSocket } from 'ws';
 
 const wss = new WebSocketServer({port: 8080});
 
-let allSockets: WebSocket[] = [];
+interface User {
+    socket: WebSocket;
+    room: string;
+}
+
+let allSockets: User[] = [];
 
 wss.on('connection', (socket) => {
-
-    allSockets.push(socket);
-
     socket.on('message', (message) => {
+        // @ts-ignore
+        const parsedMessage = JSON.parse(message);
 
-        allSockets.forEach(s => {
-            s.send(message.toString());
-        })
-    });
+        if (parsedMessage.type === 'join'){
+            allSockets.push({
+                socket,
+                room: parsedMessage.payload.roomId
+            });
+        };
+
+        if(parsedMessage.type === 'chat'){
+            
+            const currentUserRoom = allSockets.find((user) => user.socket === socket)?.room;
+
+            allSockets.forEach((user) => {
+                if (user.room === currentUserRoom) {
+                    user.socket.send(JSON.stringify({
+                        type: 'chat',
+                        payload: {
+                            message: parsedMessage.payload.message
+                        }
+                    }));
+                }
+            });
+        }
+    })
 });
